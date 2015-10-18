9d2a537e6eb66b12d497c5e53363fd6159cfc638
Odd Kristian Kvarmestøl <okknor@gmail.com>
2015-10-18 12:07:28 +0200

    Implemented ram read and write functions

--- src/ram.cpp
+++ src/ram.cpp
@@ -10,8 +10,43 @@ void ram::write(dword address, byte value) {
 
 void ram::write(dword address, dword value) {
     if(address + 4 < MEMORY_SIZE) {
-	//TODO: implement thing here      
+		*((dword*)&data[address])  = value;
     } else {
 	//TODO: add interupt call here for invalid address
     }
+}
+
+void ram::write(dword address, int value) {
+    if(address + 4 < MEMORY_SIZE) {
+		*((int*)&data[address])  = value;
+    } else {
+	//TODO: add interupt call here for invalid address
+    }
+}
+
+byte ram::read_byte(dword address) {
+	if(address < MEMORY_SIZE) {
+		return data[address];
+	} else {
+	//TODO: add interupt call here for invalid address
+		return 0;
+	}
+}
+
+dword ram::read_dword(dword address) {
+	if(address < MEMORY_SIZE) {
+		return *((dword*)&data[address]);
+	} else {
+	//TODO: add interupt call here for invalid address
+		return 0;
+	}
+}
+
+int ram::read_int(dword address) {
+	if(address < MEMORY_SIZE) {
+		return *((int*)&data[address]);
+	} else {
+	//TODO: add interupt call here for invalid address
+		return 0;
+	}
 }
